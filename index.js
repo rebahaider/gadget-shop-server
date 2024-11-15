6db6fdc04fca7a097a1cda5a7cbcbbcd30ec71db
rebahaider <rebahaiderema@gmail.com>
2024-11-16 00:28:23 +0600

    creating jwt token using email

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken")
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json())
+
+// mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aauiduw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect()
+    console.log("Database connected successfully");
+
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+}
+dbConnect();
+
+// api
+
+app.get("/", (req, res) => {
+
+  res.send("Server is running");
+});
+
+// jwt
+app.post('/authentication', async (req, res) => {
+  const useEmail = req.body;
+  const token = jwt.sign(useEmail, process.env.ACCESS_KEY_TOKEN, { expiresIn: '10d' });
+  res.send({ token });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port, ${port}`);
+});
